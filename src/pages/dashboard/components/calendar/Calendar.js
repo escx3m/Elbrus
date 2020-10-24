39d2a790607102f295b4578a108d3b6875630481
Mergen Narminov <mergen.narminov@visitech.ru>
2020-10-24 10:48:00 +0300

    fix: dashboard

--- src/pages/dashboard/components/calendar/Calendar.js
+++ src/pages/dashboard/components/calendar/Calendar.js
@@ -1,70 +1,80 @@
-import React, { Component } from 'react'
-import DayNames from './DayNames'
-import uuid from 'uuid/v4'
-import Week from './Week'
-import moment from 'moment/moment'
-import s from './Calendar.module.scss'
+import React, { Component } from 'react';
+import DayNames from './DayNames';
+import uuid from 'uuid/v4';
+import Week from './Week';
+import moment from 'moment/moment';
+import s from './Calendar.module.scss';
+import 'moment/locale/ru';
 
+moment.locale('ru');
 
 class Calendar extends Component {
-
-    state = {
-      selectedMonth: moment(),
-      selectedDay: moment().startOf("day"),
-      selectedMonthEvents: [
-        {
-          title: "The flower bed",
-          info: "Contents here",
-          itemStyle: "#1870dc",
-          date: moment(`${moment().year()}-${moment().month()+1}-02`, "YYYY-MM-DD"),
-        },
-        {
-          title: "Stop world water pollution",
-          info: "Have a kick off meeting with .inc company",
-          itemStyle: "#f0b518",
-          date: moment(`${moment().year()}-${moment().month()+1}-05`, "YYYY-MM-DD"),
-        },
-        {
-          title: "Light Blue Template 1.0.0 release",
-          info: "Some contents here",
-          itemStyle: "#58d777",
-          date: moment(`${moment().year()}-${moment().month()+1}-18`, "YYYY-MM-DD"),
-        },
-        {
-          title: "A link",
-          info: "",
-          itemStyle: "#f45722",
-          link: "http://www.flatlogic.com",
-          date: moment(`${moment().year()}-${moment().month()+1}-29`, "YYYY-MM-DD"),
-        },
-      ],
-      showEvents: false
-    };
+  state = {
+    selectedMonth: moment(),
+    selectedDay: moment().startOf('day'),
+    selectedMonthEvents: [
+      {
+        title: 'Мастер-класс',
+        info: 'Посетить мастер-класс',
+        itemStyle: '#1870dc',
+        date: moment(
+          `${moment().year()}-${moment().month() + 1}-02`,
+          'YYYY-MM-DD'
+        ),
+      },
+      {
+        title: 'Пройти тест',
+        info: 'В кабинете 123',
+        itemStyle: '#f0b518',
+        date: moment(
+          `${moment().year()}-${moment().month() + 1}-03`,
+          'YYYY-MM-DD'
+        ),
+      },
+      {
+        title: 'Событие 123',
+        info: 'Созвониться за 3 часа до события',
+        itemStyle: '#58d777',
+        date: moment(
+          `${moment().year()}-${moment().month() + 1}-18`,
+          'YYYY-MM-DD'
+        ),
+      },
+      {
+        title: 'Посмотреть видео',
+        info: 'Без рекламы',
+        itemStyle: '#f45722',
+        link: 'https://www.youtube.com',
+        date: moment(
+          `${moment().year()}-${moment().month() + 1}-29`,
+          'YYYY-MM-DD'
+        ),
+      },
+    ],
+    showEvents: false,
+  };
 
   previous = () => {
     this.setState({
-      selectedMonth: this.state.selectedMonth.subtract(1, "month")
+      selectedMonth: this.state.selectedMonth.subtract(1, 'month'),
     });
-  }
+  };
 
-  next = () =>  {
+  next = () => {
     this.setState({
-      selectedMonth: this.state.selectedMonth.add(1, "month")
+      selectedMonth: this.state.selectedMonth.add(1, 'month'),
     });
-  }
-  
-  renderMonthLabel = () =>  {
+  };
+
+  renderMonthLabel = () => {
     return (
       <span className={`${s.calendarItemContainer} ${s.monthLabel}`}>
-        {this.state.selectedMonth.format("MMMM YYYY")}
+        {this.state.selectedMonth.format('MMMM YYYY')}
       </span>
     );
-  }
+  };
 
-
-
-  
-  renderWeeks = () =>  {
+  renderWeeks = () => {
     const currentMonthView = this.state.selectedMonth;
     const currentSelectedDay = this.state.selectedDay;
 
@@ -72,9 +82,9 @@ class Calendar extends Component {
     let done = false;
     let previousCurrentNextView = currentMonthView
       .clone()
-      .startOf("month")
-      .subtract(1, "d")
-      .day("Sunday");
+      .startOf('month')
+      .subtract(1, 'd')
+      .day('Monday');
     let count = 0;
     let monthIndex = previousCurrentNextView.month();
 
@@ -88,45 +98,40 @@ class Calendar extends Component {
           selected={currentSelectedDay}
         />
       );
-      previousCurrentNextView.add(1, "w");
+      previousCurrentNextView.add(1, 'w');
       done = count++ > 2 && monthIndex !== previousCurrentNextView.month();
       monthIndex = previousCurrentNextView.month();
     }
     return weeks;
-  }
-
+  };
 
   render() {
-
-      return (
-        <div className={`${s.calendarRectangle}`}>
+    return (
+      <div className={`${s.calendarRectangle}`}>
         <div>
-        <section className={`${s.mainCalendar}`}>
-          <header className={`${s.calendarHeader}`}>
-            <div className={`${s.calendarRow} ${s.titleHeader}`}>
-              <i
-                className={`${s.calendarItemContainer} ${s.arrow} la la-arrow-left`}
-                onClick={this.previous}
-              />
-              <div className={`${s.calendarItemContainer} ${s.headerText}`}>
-              
-              {this.renderMonthLabel()}
+          <section className={`${s.mainCalendar}`}>
+            <header className={`${s.calendarHeader}`}>
+              <div className={`${s.calendarRow} ${s.titleHeader}`}>
+                <i
+                  className={`${s.calendarItemContainer} ${s.arrow} la la-arrow-left`}
+                  onClick={this.previous}
+                />
+                <div className={`${s.calendarItemContainer} ${s.headerText}`}>
+                  {this.renderMonthLabel()}
+                </div>
+                <i
+                  className={`${s.calendarItemContainer} ${s.arrow} la la-arrow-right`}
+                  onClick={this.next}
+                />
               </div>
-              <i 
-                className={`${s.calendarItemContainer} ${s.arrow} la la-arrow-right`} 
-                onClick={this.next} 
-              />
-            </div>
-            <DayNames />
-          </header>
-          <div className={`${s.daysContainer}`}>
-            {this.renderWeeks()}
-          </div>
-        </section>
+              <DayNames />
+            </header>
+            <div className={`${s.daysContainer}`}>{this.renderWeeks()}</div>
+          </section>
         </div>
-        </div>
-      );
-    }
+      </div>
+    );
+  }
 }
 
 export default Calendar;
--- src/pages/dashboard/components/calendar/DayNames.js
+++ src/pages/dashboard/components/calendar/DayNames.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
-import s from './Calendar.module.scss'
+import React, { Component } from 'react';
+import s from './Calendar.module.scss';
 
 class DayNames extends Component {
   render() {
     return (
       <div className={`${s.calendarRow} ${s.daysHeader}`}>
-        <span className={`${s.calendarItemContainer} ${s.dayName}`}>S</span>
-        <span className={`${s.calendarItemContainer} ${s.dayName}`}>M</span>
-        <span className={`${s.calendarItemContainer} ${s.dayName}`}>T</span>
-        <span className={`${s.calendarItemContainer} ${s.dayName}`}>W</span>
-        <span className={`${s.calendarItemContainer} ${s.dayName}`}>T</span>
-        <span className={`${s.calendarItemContainer} ${s.dayName}`}>F</span>
-        <span className={`${s.calendarItemContainer} ${s.dayName}`}>S</span>
+        <span className={`${s.calendarItemContainer} ${s.dayName}`}>Пн</span>
+        <span className={`${s.calendarItemContainer} ${s.dayName}`}>Вт</span>
+        <span className={`${s.calendarItemContainer} ${s.dayName}`}>Ср</span>
+        <span className={`${s.calendarItemContainer} ${s.dayName}`}>Чт</span>
+        <span className={`${s.calendarItemContainer} ${s.dayName}`}>Пт</span>
+        <span className={`${s.calendarItemContainer} ${s.dayName}`}>Сб</span>
+        <span className={`${s.calendarItemContainer} ${s.dayName}`}>Вс</span>
       </div>
     );
   }
